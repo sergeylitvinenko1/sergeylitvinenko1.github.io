f94002ac0c85c16b55edc503c1a4882ac992c300
sergeylitvinenko1 <113466645+sergeylitvinenko1@users.noreply.github.com>
2024-02-16 19:56:15 +0200

    deployment

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,7 +14,12 @@ export default function Page() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={`${montserrat.className} sm:text-xl text-lg max-w-3xl leading-relaxed`}>
-            Sergey Litvinenko is currently pursuing a master's degree in Quantitative Finance at the Vienna University of Economics and Business. Previously, he obtained his undergraduate double degree in Mathematics and Economics at the American University in Bulgaria, receiving Outstanding Achievement in Economics award. During his studies, he completed an off-cycle internship at the asset management firm Expat Capital and participated in the 2020 CFA Institute Research Challenge. Upon graduation, he joined the Deal Advisory department of KPMG Ukraine to work on valuation projects and M&A transactions, gaining valuable analytical and research skills. In 2021, Sergey passed CFA Exam Level II.
+            Sergey Litvinenko is currently pursuing his master's degree in Quantitative Finance at the Vienna University of Economics and Business. 
+            Previously, he obtained his undergraduate double degree in Mathematics and Economics at the American University in Bulgaria, 
+            receiving Outstanding Achievement in Economics award. During his studies, he completed an off-cycle internship at the asset management firm Expat Capital 
+            and participated in the 2020 CFA Institute Research Challenge. Upon graduation, he joined the Deal Advisory department of KPMG Ukraine to work on valuation projects and M&A transactions, 
+            gaining valuable analytical and research skills. While obtaining his gradute degree, Sergey worked part-time at the Institutional Clients East Department of RBI AG in Vienna, 
+            supporting bank and non-bank FIs across wide range of services. In 2021, Sergey passed CFA Exam Level II and is3 planning to enroll for Level III in 2025.
         </h1>
         <div className="flex relative top-5 justify-center gap-4 grid-cols-3 grid-rows-1"> 
           <div className="justify-center items-end flex flex-1 flex-row">
