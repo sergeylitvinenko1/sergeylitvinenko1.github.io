36875706a458b09adeda4722703079d532a3567a
sergeylitvinenko1 <113466645+sergeylitvinenko1@users.noreply.github.com>
2024-02-19 14:29:48 +0200

    animation added

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import styles from '@/app/ui/home.module.css';
 import { montserrat } from '@/app/ui/fonts';
 import Image from 'next/image';
 import { Metadata } from 'next';
-
+import { motion } from 'framer-motion'
 import Link from 'next/link';
 
 export const metadata: Metadata = {
@@ -13,9 +13,23 @@ export default function Page() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={`${montserrat.className} text-xl md:text-3xl md:leading-normal`}>
-          My name is Sergey Litvinenko
-        </h1>
+        <motion.div initial="hidden" animate="visible" variants={{
+          hidden: {
+            scale: .8,
+            opacity:0
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: .4
+            }
+          },
+        }}>
+          <h1 className={`${montserrat.className} text-xl md:text-3xl md:leading-normal`}>
+            My name is Sergey Litvinenko
+          </h1>
+        </motion.div>
         <div className="flex relative top-5 justify-center gap-4 grid-cols-3 grid-rows-1"> 
           <div className="justify-center items-end flex flex-1 flex-row">
             <div className="text-center">
